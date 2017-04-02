74c2ff8743431309cf5c4c02d9fe6bf53cdd87bc
Matthew <mrk45@uclive.ac.nz>
2017-04-03 00:18:51 +1200

    Improvements

---
+++ Readme.md
@@ -0,0 +1,15 @@
+## Pester
+Pesters someone to get their attention.
+
+#### Installation
+```shell
+/kpm install pester
+```
+
+#### Configuration
+```shell
+/kpm config pester emailConfig <nodemailer config>
+/kpm config pester smsConfig {"phoneNumber":"<twilioPhoneNumber>", "sid":"<twilioSID>", "token":"<twilioToken>"}
+```
+
+See [https://twilio.github.io/twilio-node](https://twilio.github.io/twilio-node) and [https://nodemailer.com/](https://nodemailer.com/).
--- pester.js
+++ pester.js
@@ -4,10 +4,11 @@ const argsParser = require('concierge/arguments'),
 	twilio = require('twilio');
 
 let mailTransport = null,
-	twilioTransport = null;
+	twilioTransport = null,
+	facebookHandle = null;
 	
-const privateMessage = (api, user, message) => {
-	api.sendPrivateMessage(message, user.id);
+const privateMessage = (api, event, user, message) => {
+	api.sendPrivateMessage(`${user.name}, ${event.sender_name} says: ${message}`, user.id);
 };
 
 const textMessage = (phoneNumber, user, message) => {
@@ -23,7 +24,13 @@ const textMessage = (phoneNumber, user, message) => {
 };
 
 const seenChat = (api, event, user, message) => {
-	
+	exports.config.seen.push({
+		id: user.id,
+		thread: event.thread_id,
+		name: user.name,
+		from: event.sender_name,
+		message: message
+	});
 };
 
 const respondChat = (api, event, user, message) => {
@@ -55,8 +62,33 @@ const onMessage = (api, event) => {
 		if (event.thread_id === pester.thread && event.sender_id === pester.id) {
 			exports.config.respond.splice(i, 1);
 			i--;
-			api.sendMessage(`@${pester.name}, @{pester.from} says: ${pester.message}`, event.thread_id);
+			api.sendMessage(`@${pester.name}, @${pester.from} says: ${pester.message}`, event.thread_id);
 		}
+	}
+};
+
+const onRead = readObj => {
+	for (let i = 0; i < exports.config.seen.length; i++) {
+		const pester = exports.config.seen[i];
+		if (readObj.thread_id === pester.thread && readObj.sender_id === pester.id) {
+			exports.config.seen.splice(i, 1);
+			i--;
+			facebookHandle.getApi().sendMessage(`@${pester.name}, @${pester.from} says: ${pester.message}`, readObj.thread_id);
+		}
+	}
+};
+
+const onStart = status => {
+	if (status.success && !facebookHandle && status.integration.__descriptor.name === 'facebook') {
+		facebookHandle = status.integration;
+		facebookHandle.on('read', onRead);
+	}
+};
+
+const onPreStop = integ => {
+	if (facebookHandle && integ.__descriptor.name === 'facebook') {
+		facebookHandle.removeListener('read', onRead);
+		facebookHandle = null;
 	}
 };
 
@@ -83,21 +115,36 @@ exports.load = platform => {
 	else {
 		LOG.warn('SMS settings have not been configured');
 	}
+	
+	facebookHandle = platform.modulesLoader.getLoadedModules('integration')
+		.find(i => i.__descriptor.name === 'facebook' && i.__descriptor.__running);
+	if (facebookHandle) {
+		facebookHandle.on('read', onRead);
+	}
+	
+	platform.modulesLoader.on('start', onStart);
+	platform.modulesLoader.on('prestop', onPreStop);
 };
 
 exports.unload = platform => {
+	platform.modulesLoader.removeListener('start', onStart);
+	platform.modulesLoader.removeListener('prestop', onPreStop);
+	if (facebookHandle) {
+		facebookHandle.removeListener('read', onRead);
+	}
+	facebookHandle = null;
 	platform.removeListener('message', onMessage);
 };
 
 exports.run = (api, event) => {
 	const pesterQueue = [];
-	const args = [
+	const pesterArgs = [
 		{
 			long: '--private',
 			short: '-pm',
 			description: 'Sends a private message to the person on this integration.',
 			run: () => {
-				pesterQueue.push(privateMessage.bind(this, api));
+				pesterQueue.push(privateMessage.bind(this, api, event));
 			}
 		},
 		{
@@ -150,44 +197,56 @@ exports.run = (api, event) => {
 			}
 		}
 	];
-	
+
+	let args;
 	try {
-        const args = argsParser.parseArguments(event.arguments.slice(1), args, {
+        args = argsParser.parseArguments(event.arguments.slice(1), pesterArgs, {
             enabled: true,
-            string: `${api.commandPrefix}pester <name/id> <message...>`,
+            string: `${api.commandPrefix}pester [name/id] [message...]`,
             colours: false
         });
     }
     catch (e) {
         return api.sendMessage(e.message, event.thread_id);
     }
-		
+
+	if (args.parsed['-h']) {
+		return api.sendMessage(args.parsed['-h'].out, event.thread_id);
+	}
+
 	if (args.unassociated.length < 2) {
 		return api.sendMessage('Both a name/id and a message must be provided.', event.thread_id);
 	}
-	
+
 	const message = args.unassociated.slice(1).join(' ');
 	if (args.parsed['-tx'] && message.length > 160) {
 		return api.sendMessage('The maximum length of a text message is 160 characters. Please shorten your message.', event.thread_id);
 	}
-	
+
 	const inputUser = args.unassociated[0].trim().toLowerCase();
 	const users = api.getUsers(event.thread_id);
 	const searchResults = Object.keys(users).filter(u => {
 		const usr = users[u];
 		const t = u.trim().toLowerCase();
-		return t.startsWith(inputUser) || usr.name.trim().toLowerCase().startsWith(inputUser) || usr.id === inputUser;
+		return t.startsWith(inputUser) || usr.name.trim().toLowerCase().startsWith(inputUser) || usr.id === inputUser || u === inputUser;
 	});
-	
-	if (searchResults.length > 0) {
-		return api.sendMessage(`Don't know who you mean by "${args.unassociated[0]}":\n- ${searchResults.join('\n- ')}`, event.thread_id);
+
+	if (searchResults.length > 1) {
+		return api.sendMessage(`Don't know who you mean by "${args.unassociated[0]}":\n- ${searchResults.map(u => `${u}: ${users[u].name}`).join('\n- ')}`, event.thread_id);
 	}
 	else if (searchResults === 0) {
 		return api.sendMessage(`Ummmm, well... this is akward. Who is ${args.unassociated[0]}?`, event.thread_id);
 	}
-	
-	api.sendMessage('Pester in progress...', event.thread_id);
-	const user = users[0];
+
+	if (pesterQueue.length === 0) {
+		return api.sendMessage('I would pester, but you never told me how...', event.thread_id);
+	}
+
+	const user = users[searchResults[0]];
+	if (!user.id) {
+		user.id = searchResults[0];
+	}
+	api.sendMessage(`Pestering ${user.name} with the message "${message}". Options: ${Object.keys(args.parsed).join(', ')}.`, event.thread_id);
 	for (let pester of pesterQueue) {
 		pester(user, message);
 	}
